a7f61833097823b35f3c8c1c68f11e823f657fc6
Jeremy Magland <jmagland@flatironinstitute.org>
2025-04-22 09:24:54 -0400

    api base url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:3000/api';
+// export const API_BASE_URL = 'http://localhost:3000/api';
+export const API_BASE_URL = "https://dandi-notebook-review-api.vercel.app";
 
 export const createHeaders = (userEmail: string, apiToken: string) => ({
   'Content-Type': 'application/json',
